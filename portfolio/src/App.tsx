997988611fd97e49f51f263d35b73ce56aa7f698
JoshBenn <sublimeibanez@protonmail.com>
2024-04-07 13:52:17 -0700

    asd;fljk

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -1,4 +1,4 @@
-import './App.css';
+import "./App.css";
 
 import Header from "./components/Header";
 import About from "./components/About";
--- portfolio/src/components/Header.tsx
+++ portfolio/src/components/Header.tsx
@@ -25,12 +25,10 @@ export default function Header() {
     return (
         <>
             <div className="flex flex-col">
-                <a className="text-5xl min-w-full -pr-4" href="#">
+                <a className="text-5xl" href="#">
                     Joshua Benn
                 </a>
-                <div className="-mt-5 z-10 min-w-80 max-w-80">
-                    <img src={Underline} />
-                </div>
+                <img src={Underline} className="-mt-5 z-10 w-[330px]" />
             </div>
             <div className="flex sm:flex-col justify-between">
                 <div className="social flex gap-3 mt-1">
