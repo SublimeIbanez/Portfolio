cc2c301c15c9e075d251593ed5cc725a1a474729
JoshBenn <sublimeibanez@protonmail.com>
2024-10-14 19:00:39 -0500

    Updated About

--- portfolio/src/components/Content/About.tsx
+++ portfolio/src/components/Content/About.tsx
@@ -9,7 +9,8 @@ export default function About() {
             <hr className="h-1 bg-gray-100 border-0 rounded dark:bg-gray-700" />
             <p>
                 I am a 7 year veteran of the United States Air Force and a graduate of Arizona State University with a degree in Software Engineering.
-                I have experience working computer systems and familiarity with solving IT and software related issues.
+                Experienced Information Technology Operations and Information Technology Technician with a demonstrated history of working in the Defense & Space industry.
+                Skilled in requirements analysis, enterprise software, government, software implementation, and security with strong quality assurance.
             </p>
 
         </div>
